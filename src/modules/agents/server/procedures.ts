e506a32d571f5750b2aae600d89fd9e11fb6792a
Sanwed <kozyukov.alex@gamil.com>
2025-07-05 16:36:51 +0400

    type fix

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -7,7 +7,12 @@ import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
   getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(agents);
+    const data = await db
+      .select({
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
+      .from(agents);
 
     return data;
   }),
